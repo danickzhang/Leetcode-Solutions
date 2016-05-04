006f0bb322e75fe4c2552f7deea1266d6fe8f3df
Chen Zhang <zhang.danick@gmail.com>
2016-05-04 15:59:44 -0500

    Update according to question description
    the old comparison to Integer.MAX or MIN is meaningless

--- Reverse Integer/ReverseInteger.java
+++ Reverse Integer/ReverseInteger.java
@@ -1,12 +1,12 @@
 public class Solution {
     public int reverse(int x) {
-        if (x > Integer.MAX_VALUE || x < Integer.MIN_VALUE)
-            return 0;
-        int rtn = 0;
+        long rtn = 0;
         while (x != 0) {
             rtn = rtn * 10 + x % 10;
             x = x / 10;
         }
-        return rtn;
+        
+        if(Math.abs(rtn) > Integer.MAX_VALUE) return 0;
+        else return (int) rtn;
     }
 }
